266a069507d1b3315ab5a14b9fe261e4b08d294c
Nikita Zytsev <eronmcgonagal@gmail.com>
2024-02-16 00:14:39 +0300

    fix

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,12 +2,14 @@ import Business from '@/components/Business/Business';
 import Course from '@/components/Course/Course';
 import Create from '@/components/Create/Create';
 import Header from '@/components/Header/Header';
+import MainScreen from '@/components/MainScreen/MainScreen';
 import NextBot from '@/components/NextBot/NextBot';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <MainScreen />
       <Business />
       <Create />
       <NextBot />
