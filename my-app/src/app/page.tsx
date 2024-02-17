60d2592c38da1e1cf9ff0fd96fa5af82911ea6f0
HellEaglee <helleagleee@gmail.com>
2024-02-17 14:37:53 +0300

    Feat: links added

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,12 @@
-import Business from '@/components/Business/Business';
-import Course from '@/components/Course/Course';
-import Create from '@/components/Create/Create';
-import Header from '@/components/Header/Header';
-import Help from '@/components/Help/Help';
-import MainScreen from '@/components/MainScreen/MainScreen';
-import NextBot from '@/components/NextBot/NextBot';
+import Business from "@/components/Business/Business";
+import Course from "@/components/Course/Course";
+import Create from "@/components/Create/Create";
+import Header from "@/components/Header/Header";
+import Help from "@/components/Help/Help";
+import Links from "@/components/Links/Links";
+import MainScreen from "@/components/MainScreen/MainScreen";
+import NextBot from "@/components/NextBot/NextBot";
+import Tryit from "@/components/Tryit/Tryit";
 
 export default function Home() {
   return (
@@ -16,6 +18,8 @@ export default function Home() {
       <NextBot />
       <Course />
       <Help />
+      <Tryit />
+      <Links />
     </div>
   );
 }
