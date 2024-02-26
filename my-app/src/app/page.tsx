6e27a9117aecbc6463c10aa04c10900985730de1
HellEaglee <helleagleee@gmail.com>
2024-02-26 14:37:35 +0300

    Feat: fixes

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,32 +1,32 @@
-import Steps from '@/components/5Steps/Steps';
-import Base from '@/components/Base/Base';
-import Business from '@/components/Business/Business';
-import Course from '@/components/Course/Course';
-import Create from '@/components/Create/Create';
-import Footer from '@/components/Footer/Footer';
-import Header from '@/components/Header/Header';
-import Help from '@/components/Help/Help';
-import II from '@/components/II/II';
-import Links from '@/components/Links/Links';
-import MainScreen from '@/components/MainScreen/MainScreen';
-import NextBot from '@/components/NextBot/NextBot';
-import Tryit from '@/components/Tryit/Tryit';
-import Payments from '@/components/payments/Payments';
+import Steps from "@/components/5Steps/Steps";
+import Base from "@/components/Base/Base";
+import Business from "@/components/Business/Business";
+import Course from "@/components/Course/Course";
+import Create from "@/components/Create/Create";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import Help from "@/components/Help/Help";
+import II from "@/components/II/II";
+import Links from "@/components/Links/Links";
+import MainScreen from "@/components/MainScreen/MainScreen";
+import NextBot from "@/components/NextBot/NextBot";
+import Tryit from "@/components/Tryit/Tryit";
+import Payments from "@/components/payments/Payments";
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center overflow-hidden'>
-      <div className=' xl:max-w-[1280px] xm:max-w-[1400px] md:max-w-[768px] xx:max-w-[1577px] xb:max-w-[1768px] lg:max-w-[1024px] bb:max-w-[1920px] '>
+    <div className="flex flex-col items-center overflow-hidden">
+      <div className=" xl:max-w-[1280px] xm:max-w-[1400px] xx:max-w-[1577px] xb:max-w-[1768px] lg:max-w-[1024px]  ">
         <Header />
         <MainScreen />
-        <Create id={0} title={''} />
+        <Create id={0} title={""} />
         <NextBot />
         <Course />
         <Help />
         <Tryit />
         <Links />
         <Base />
-        <div className='hidden'>
+        <div className="hidden">
           <II />
         </div>
         <Steps />
--- my-app/src/components/Tryit/Tryit.tsx
+++ my-app/src/components/Tryit/Tryit.tsx
@@ -1,27 +1,27 @@
-'use client';
-import tabs from '@/Images/tabs.svg';
-import { useState } from 'react';
+"use client";
+import tabs from "@/Images/tabs.svg";
+import { useState } from "react";
 
 const TabContent = [
   {
-    title: 'ИИ-бот на основе ChatGPT 3,5 Turbo и ChatGPT 4 Turbo',
+    title: "ИИ-бот на основе ChatGPT 3,5 Turbo и ChatGPT 4 Turbo",
     imageSrc:
-      'https://www.figma.com/file/2xzC7PiYlt9Zkqn9M7sAST/image/34eda0e497db142bfac37284807115474f9d0027',
+      "https://www.figma.com/file/2xzC7PiYlt9Zkqn9M7sAST/image/34eda0e497db142bfac37284807115474f9d0027",
   },
   {
-    title: 'Настройка без знаний программирования',
+    title: "Настройка без знаний программирования",
     imageSrc:
-      'https://www.figma.com/file/2xzC7PiYlt9Zkqn9M7sAST/image/34eda0e497db142bfac37284807115474f9d0027',
+      "https://www.figma.com/file/2xzC7PiYlt9Zkqn9M7sAST/image/34eda0e497db142bfac37284807115474f9d0027",
   },
   {
-    title: 'Все основные настройки вынесены в интерфейс сервиса',
+    title: "Все основные настройки вынесены в интерфейс сервиса",
     imageSrc:
-      'https://www.figma.com/file/2xzC7PiYlt9Zkqn9M7sAST/image/34eda0e497db142bfac37284807115474f9d0027',
+      "https://www.figma.com/file/2xzC7PiYlt9Zkqn9M7sAST/image/34eda0e497db142bfac37284807115474f9d0027",
   },
   {
-    title: 'Понятное объяснение для каждого пункта настроек',
+    title: "Понятное объяснение для каждого пункта настроек",
     imageSrc:
-      'https://www.figma.com/file/2xzC7PiYlt9Zkqn9M7sAST/image/34eda0e497db142bfac37284807115474f9d0027',
+      "https://www.figma.com/file/2xzC7PiYlt9Zkqn9M7sAST/image/34eda0e497db142bfac37284807115474f9d0027",
   },
 ];
 
@@ -29,68 +29,68 @@ function Tryit() {
   const [activeTab, setActiveTab] = useState(0);
   return (
     <>
-      <section className='flex flex-col xm:px-[10px] items-center md:items-start md:ml-[50px] lg:ml-0 lg:justify-center bb:max-w-[1920px] mt-[80px] xx:mt-[180px] md:mt-[60px]'>
-        <div className='flex items-end pb-[14px] border-b-2 border-blue xx:w-[1577px] md:w-[668px] md:pb-2'>
-          <h2 className='max-w-[292px] text-[22px] leading-[26.4px] text-center font-Regular md:max-w-[531px] md:leading-[21.6px] md:text-[18px] md:text-start xx:max-w-[1576px] lg:text-start lg:px-2 lg:max-w-[1576px] xx:text-start xx:text-[36px] xx:leading-[43.2px] xx:pl-[150px] bb:pl-[0]'>
-            В NextBot вы создадите бота с искусственным интеллектом. Он общается{' '}
-            <br className='hidden xx:flex' />
+      <section className="flex flex-col xm:px-[10px] items-center md:items-center md:px-[50px] lg:ml-0 lg:justify-center bb:max-w-[1920px] mt-[80px] xx:mt-[180px] md:mt-[60px]">
+        <div className="flex items-center pb-[14px] border-b-2 border-blue md:w-full xx:w-[1577px] md:pb-2">
+          <h2 className="max-w-[292px] text-[22px] leading-[26.4px] text-center font-Regular md:max-w-[531px] md:leading-[21.6px] md:text-[18px] md:text-start xx:max-w-[1576px] lg:text-start lg:px-2 lg:max-w-[1576px] xx:text-start xx:text-[36px] xx:leading-[43.2px] xx:pl-[150px] bb:pl-[0]">
+            В NextBot вы создадите бота с искусственным интеллектом. Он общается{" "}
+            <br className="hidden xx:flex" />
             как человек. Клиенты не отличают его от настоящего консультанта
           </h2>
         </div>
-        <div className='flex flex-col items-center pt-5 md:hidden'>
-          <p className='text-[16px] leading-[20.8px] text-center font-Regular pb-5 md:text-xl'>
+        <div className="flex flex-col items-center pt-5 md:hidden">
+          <p className="text-[16px] leading-[20.8px] text-center font-Regular pb-5 md:text-xl">
             Без сложных настроек
           </p>
-          <div className='flex flex-wrap justify-center items-center gap-5 w-full'>
+          <div className="flex flex-wrap justify-center items-center gap-5 w-full">
             {[
-              'ИИ-бот на основе ChatGPT 3,5 Turbo и ChatGPT 4 Turbo',
-              'Настройка без знаний программирования',
-              'Все основные настройки вынесены в интерфейс сервиса',
-              'Понятное объяснение для каждого пункта настроек',
+              "ИИ-бот на основе ChatGPT 3,5 Turbo и ChatGPT 4 Turbo",
+              "Настройка без знаний программирования",
+              "Все основные настройки вынесены в интерфейс сервиса",
+              "Понятное объяснение для каждого пункта настроек",
             ].map((item, index) => (
-              <div key={index} className='flex flex-col items-center gap-[5px]'>
-                <div className='w-[300px] h-[52px] flex items-center justify-center bg-blue rounded-[10px]'>
+              <div key={index} className="flex flex-col items-center gap-[5px]">
+                <div className="w-[300px] h-[52px] flex items-center justify-center bg-blue rounded-[10px]">
                   <p
                     className={`h-[42px] text-[16px] leading-[20.8px] text-center text-Regular text-white
-                  ${index === 0 || 1 ? 'max-w-[230px]' : ''}
-                  ${index === 2 || 3 ? 'max-w-[260px]' : ''}
+                  ${index === 0 || 1 ? "max-w-[230px]" : ""}
+                  ${index === 2 || 3 ? "max-w-[260px]" : ""}
                   `}
                   >
                     {item}
                   </p>
                 </div>
-                <div className='w-[300px] h-[218px] p-[17px] flex items-center justify-center bg-cloud rounded-[10px]'>
+                <div className="w-[300px] h-[218px] p-[17px] flex items-center justify-center bg-cloud rounded-[10px]">
                   <img
-                    src='https://s3-alpha-sig.figma.com/img/34ed/a0e4/97db142bfac37284807115474f9d0027?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K0AczAYtdLUuAHnn3YGcQgZtoFSv46wJ5CVSgPMZGp50CnLUooiHi-P50dXj12zvkFKtmmjOJ6qju5FfgrBPBpiHMLskV1U-sS2nfoOd3laO0jqPi~8QNyAvxtQxPNQ~UvTpzBEWCl8WWLg~DqfkAeJdUDnFoAN~uNjRrdUcX8WFTuJQZnjFEdjmf7A121CcB7NiRfd7C1Hk-lkZxIsQKAUrI1tSEGKbwsa91p19~JteuHr0B9ffAzuN4qan981ztMdvBgEtfga2GDOAHjQo9q--EZuMP2V-Qe4Zq3Ioj66KJiD3RnHVK4s7UFlUrdYX2B84QyYG5aR1ubK2eTvWPg__'
-                    alt='TryitExampleIMG'
-                    className='rounded-[10px]'
+                    src="https://s3-alpha-sig.figma.com/img/34ed/a0e4/97db142bfac37284807115474f9d0027?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K0AczAYtdLUuAHnn3YGcQgZtoFSv46wJ5CVSgPMZGp50CnLUooiHi-P50dXj12zvkFKtmmjOJ6qju5FfgrBPBpiHMLskV1U-sS2nfoOd3laO0jqPi~8QNyAvxtQxPNQ~UvTpzBEWCl8WWLg~DqfkAeJdUDnFoAN~uNjRrdUcX8WFTuJQZnjFEdjmf7A121CcB7NiRfd7C1Hk-lkZxIsQKAUrI1tSEGKbwsa91p19~JteuHr0B9ffAzuN4qan981ztMdvBgEtfga2GDOAHjQo9q--EZuMP2V-Qe4Zq3Ioj66KJiD3RnHVK4s7UFlUrdYX2B84QyYG5aR1ubK2eTvWPg__"
+                    alt="TryitExampleIMG"
+                    className="rounded-[10px]"
                   />
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <div className='hidden xx:flex pl-[100px] bb:pl-[259px] justify-center items-start pt-10 w-[1577px] gap-[44px] '>
+        <div className="hidden xx:flex pl-[100px] bb:pl-[259px] justify-center items-start pt-10 w-[1577px] gap-[44px] ">
           <div>
-            <p className='text-[22px] leading-[28.6px] font-Regular text-dark'>
+            <p className="text-[22px] leading-[28.6px] font-Regular text-dark">
               Без сложных настроек
             </p>
-            <div className='flex flex-col gap-[10px] pt-5'>
+            <div className="flex flex-col gap-[10px] pt-5">
               {TabContent.map((item, index) => (
                 <div
                   key={index}
                   className={`max-w-[380px] h-[68px] border-[1px] border-blue rounded-[10px] flex justify-start font-Regular items-center transition-all duration-500 pl-[11px] pt-[10px] pb-[6px] cursor-pointer ${
                     activeTab === index
-                      ? 'bg-blue text-white'
-                      : 'bg-white text-dark'
+                      ? "bg-blue text-white"
+                      : "bg-white text-dark"
                   }`}
                   onClick={() => setActiveTab(index)}
                 >
                   <p
                     className={`text-xl leading-[26px] font-Regular ${
-                      index === 0 && 'max-w-[286px]'
-                    } ${index === 2 && 'max-w-[306px]'} ${
-                      index === 3 && 'max-w-[325px]'
+                      index === 0 && "max-w-[286px]"
+                    } ${index === 2 && "max-w-[306px]"} ${
+                      index === 3 && "max-w-[325px]"
                     }`}
                   >
                     {item.title}
@@ -99,41 +99,41 @@ function Tryit() {
               ))}
             </div>
           </div>
-          <div className='relative flex pt-[150px] pr-[24px] '>
+          <div className="relative flex pt-[150px] pr-[24px] ">
             <img
               src={tabs.src}
-              alt='icon'
-              className='rotate-180 absolute top-[135px] right-[-3px]'
+              alt="icon"
+              className="rotate-180 absolute top-[135px] right-[-3px]"
             />
-            <img src={tabs.src} alt='icon' />
+            <img src={tabs.src} alt="icon" />
           </div>
           <img
             src={TabContent[activeTab].imageSrc}
-            alt='Tab Content'
-            className='w-[780px] h-[534px] rounded-[10px] py-[29px] px-[42px] bg-cloud mt-[50px]'
+            alt="Tab Content"
+            className="w-[780px] h-[534px] rounded-[10px] py-[29px] px-[42px] bg-cloud mt-[50px]"
           />
         </div>
-        <div className='hidden mx-3 lg:flex xx:hidden lg:max-w-[1224px] justify-end items-start pt-10 w-fit gap-[20px] '>
+        <div className="hidden mx-3 lg:flex xx:hidden lg:max-w-[1224px] justify-end items-start pt-10 w-fit gap-[20px] ">
           <div>
-            <p className='text-[22px] leading-[28.6px] font-Regular text-dark'>
+            <p className="text-[22px] leading-[28.6px] font-Regular text-dark">
               Без сложных настроек
             </p>
-            <div className='flex flex-col gap-[10px] pt-5'>
+            <div className="flex flex-col gap-[10px] pt-5">
               {TabContent.map((item, index) => (
                 <div
                   key={index}
                   className={`max-w-[380px] h-[68px] border-[1px] border-blue rounded-[10px] flex justify-start font-Regular items-center transition-all duration-500 pl-[11px] pt-[10px] pb-[6px] cursor-pointer ${
                     activeTab === index
-                      ? 'bg-blue text-white'
-                      : 'bg-white text-dark'
+                      ? "bg-blue text-white"
+                      : "bg-white text-dark"
                   }`}
                   onClick={() => setActiveTab(index)}
                 >
                   <p
-                    className={`text-xl leading-[26px] font-Regular ${
-                      index === 0 && 'max-w-[286px]'
-                    } ${index === 2 && 'max-w-[306px]'} ${
-                      index === 3 && 'max-w-[325px]'
+                    className={`text-[18px] leading-[22px] font-Regular ${
+                      index === 0 && "max-w-[286px]"
+                    } ${index === 2 && "max-w-[306px]"} ${
+                      index === 3 && "max-w-[325px]"
                     }`}
                   >
                     {item.title}
@@ -142,33 +142,33 @@ function Tryit() {
               ))}
             </div>
           </div>
-          <div className='relative flex pt-[150px] pr-[24px] '>
+          <div className="relative flex pt-[150px] pr-[24px] ">
             <img
               src={tabs.src}
-              alt='icon'
-              className='rotate-180 absolute top-[135px] right-[-3px]'
+              alt="icon"
+              className="rotate-180 absolute top-[135px] right-[-3px]"
             />
-            <img src={tabs.src} alt='icon' />
+            <img src={tabs.src} alt="icon" />
           </div>
           <img
             src={TabContent[activeTab].imageSrc}
-            alt='Tab Content'
-            className='w-[580px] h-[434px] rounded-[10px] py-[29px] px-[42px] bg-cloud mt-[50px]'
+            alt="Tab Content"
+            className="w-[580px] h-[434px] rounded-[10px] py-[29px] px-[42px] bg-cloud mt-[50px]"
           />
         </div>
-        <div className='hidden md:flex lg:hidden justify-end items-start pt-6 w-fit gap-[31px] '>
+        <div className="hidden md:flex lg:hidden justify-end items-start pt-6 w-fit gap-[31px] ">
           <div>
-            <p className='text-[14px] leading-[18.2px] font-Regular text-dark'>
+            <p className="text-[14px] leading-[18.2px] font-Regular text-dark">
               Без сложных настроек
             </p>
-            <div className='flex flex-col gap-[5px] pt-[2px]'>
+            <div className="flex flex-col gap-[5px] pt-[2px]">
               {TabContent.map((item, index) => (
                 <div
                   key={index}
                   className={`max-w-[196px] h-[42px] border-[1px] border-blue rounded-[10px] flex justify-start font-Regular items-center transition-all duration-500 pl-[11px] pt-[10px] pb-[6px] cursor-pointer ${
                     activeTab === index
-                      ? 'bg-blue text-white'
-                      : 'bg-white text-dark'
+                      ? "bg-blue text-white"
+                      : "bg-white text-dark"
                   }`}
                   onClick={() => setActiveTab(index)}
                 >
@@ -179,22 +179,22 @@ function Tryit() {
               ))}
             </div>
           </div>
-          <div className='relative flex pt-[40px] pr-[5px]'>
+          <div className="relative flex pt-[40px] pr-[5px]">
             <img
               src={tabs.src}
-              alt='icon'
-              className='rotate-180 absolute top-[35px] right-[-3px] w-[13.8px] h-2 object-cover'
+              alt="icon"
+              className="rotate-180 absolute top-[35px] right-[-3px] w-[13.8px] h-2 object-cover"
             />
             <img
               src={tabs.src}
-              alt='icon'
-              className='w-[13.8px] h-2 object-cover'
+              alt="icon"
+              className="w-[13.8px] h-2 object-cover"
             />
           </div>
           <img
             src={TabContent[activeTab].imageSrc}
-            alt='Tab Content'
-            className='w-[386px] h-[264px] rounded-[4px] py-[15px] px-[21px] bg-cloud mt-[20px] ml-1'
+            alt="Tab Content"
+            className="w-[386px] h-[264px] rounded-[4px] py-[15px] px-[21px] bg-cloud mt-[20px] ml-1"
           />
         </div>
       </section>
